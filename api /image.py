fb4e54018a853d17f83208245e4ef6d20d9fe1b9
JeBROcookie <145489490+JeBROcookie@users.noreply.github.com>
2025-02-06 18:50:46 +0100

    Update image.py

--- api /image.py
+++ api /image.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://tenor-gules-delta.vercel.app/api/picture" # Link to the webpage to redirect to 
+        "page": "https://tenor-gules-delta.vercel.app/api/image" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
